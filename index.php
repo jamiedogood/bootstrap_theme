4969c6c7e2bfc23a9b7856122f6d401c209f7ed1
James Kwezi <james@silverback-it.be>
2016-10-19 14:30:57 +0200

    clearfix class for uneven column heights

--- index.php
+++ index.php
@@ -24,6 +24,40 @@
         For but only a fleeting moment
       </p>
     </div>
+    <div class="container">
+      <div class="row bg-color">
+        <div class="col-md-3 col-sm-6">
+          <h3>Our Services</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button class="btn btn-primary">More</button>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h3>Full time Development</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <button class="btn btn-primary">More</button>
+        </div>
+        <div class="clearfix visible-sm-block"></div>
+
+        <div class="col-md-3 col-sm-6">
+          <h3>Serverside Rendering</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button class="btn btn-primary">More</button>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h3>Web development</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button class="btn btn-primary">More</button>
+        </div>
+      </div>
+    </div>
 
 
 
